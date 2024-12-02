559a361f6dfdb6b123df3b2a256b994b3b3a2de7
welizaa <wmss6@aluno.ifal.edu.br>
2024-12-02 20:13:29 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let num = parseFloat(prompt('Digite um numero: '));
+
+if (num < 0) {
+    alert("Voce digitou um numero negativo");
+} else {
+    
+    alert("Voce digitou um numero positivo");
+}
